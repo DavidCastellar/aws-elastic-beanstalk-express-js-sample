40f0e445b24f1f298677bc06a58c0537004331bc
DavidSadracCastellar <david.castellar@agswebconsulting.com>
2021-04-08 16:33:00 -0500

    Added new Stage Review to Pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello David, from AWS!!'));
+app.get('/', (req, res) => res.send('Hello David, from AWS!! \n Now added new Stage to Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
